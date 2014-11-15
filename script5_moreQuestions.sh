e7d830366577767f02aa44ffc8a898b206c9579c
Colby Chiang <cc2qe@virginia.edu>
2014-11-15 08:57:49 -0600

    colby script5 updates

--- script5_moreQuestions.sh
+++ script5_moreQuestions.sh
@@ -1,3 +1,5 @@
+cd ~/HALL_2014/results
+
 #----------------------------------------------------------------------------------------------------------
 # 1) HOW MANY LUMPY DELETION CALLS ARE SUPPORTED BY CNV CALLS MADE BY READ-DEPTH ANALYSIS WITH CNVNATOR?
 #----------------------------------------------------------------------------------------------------------
@@ -8,7 +10,7 @@ cat ../supplemental/NA12878.cnvnator.cnvs.bed | grep DEL \
     | bedtools intersect -a deletions.naive.bed -b stdin -r -f 0.5 -wo \
     | cut -f 4 | sort -u | wc -l
 #   35
-# so, 35 of 488 (7.2%) of the naive LUMPY calls have read-depth support 
+# so, 35 of 467 (7.5%) of the naive LUMPY calls have read-depth support 
 
 cat ../supplemental/NA12878.cnvnator.cnvs.bed | grep DEL \
     | bedtools intersect -a deletions.strict.bed -b stdin -r -f 0.5 -wo \
@@ -33,9 +35,9 @@ bedtools intersect -a deletions.strict.bed -b ../annotations/LINE.b37.sorted.bed
 #----------------------------------------------------------------------------------------------------------
 # First, go get the gene annotations file:
 # Look at the remote file using curl
-curl  -s "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz" | gunzip -c | head
+curl -s "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz" | gunzip -c | head
 # Reformat and write file, making a bed entry for each exon in the gene file
-curl  -s "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz" | gunzip -c \
+curl -s "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz" | gunzip -c \
     | awk '{gsub("^chr","",$3); n=int($9); split($10,start,",");split($11,end,","); for(i=1;i<=n;++i) {print $3,start[i],end[i],$2"."i,$13,$2}}' OFS="\t" \
     | bedtools sort > ~/HALL_2014/annotations/refGene.exons.b37.bed
 
@@ -61,11 +63,10 @@ bedtools pairtobed -a deletions.strict.bedpe -b ../annotations/refGene.exons.b37
 # You should use the entire genome callset for this question to ensure that some are found
 # ~/HALL_2014/supplemental/NA12878.lumpy.bedpe
 # Don't feel bad if you can't figure this one out; Colby had trouble with it too
-
-pairToBed -type either -a ../supplemental/NA12878.lumpy.bedpe -b refGene.exons.b37.bed \
+pairToBed -type either -a ../supplemental/NA12878.lumpy.bedpe -b ../annotations/refGene.exons.b37.bed \
     | sort -k7,7 \
-    | groupBy -g 7 -c 21,22,20,22,20 -o distinct,distinct,distinct,count_distinct,count_distinct | awk '$5==1 && $6>1'
-    
+    | groupBy -g 7 -c 21,22,20,22,20 -o distinct,distinct,distinct,count_distinct,count_distinct \
+    | awk '$5==1 && $6>1'
 ## ID   GENE    TRANSCRIPT  EXONS   NUM_TRANSCRIPTS NUM_EXONS
 # 2829  LINC00202-1 NR_026795   NR_026795.3,NR_026795.5 1   2
 # 3086  MTCH2   NM_014342   NM_014342.1,NM_014342.2 1   2
@@ -76,15 +77,37 @@ pairToBed -type either -a ../supplemental/NA12878.lumpy.bedpe -b refGene.exons.b
 # 3403  TDG NM_003211   NM_003211.10,NM_003211.9    1   2
 # 3455  FBRSL1  NM_001142641    NM_001142641.1,NM_001142641.2   1   2
 
-
 # b) Can you find "complex variants" where deletion, duplication and/or inversion calls are clustered or overlapping each other?
 # hint, use pairtopair of the variant callset file against itself, using "-rdn" to not report self-hits
+bedtools pairtopair -type either -is -rdn -a breakpoints.strict.bedpe -b breakpoints.strict.bedpe 
 
 # c) Did we capture any believable inversion calls where both breakpoints were detected?
-# hint, this information is contained in column 13 of the breakpoints.strict.bedpe file
+# hint 1: this information is contained in column 13 of the breakpoints.strict.bedpe file
 # which describes the different read-pair strand combinations that were clustered
+# hint 2: searching for the literal '-' requires a preceding backslash as an escape character ('\-')
+cat breakpoints.strict.bedpe | grep '++' | grep '\-\-'
+# the backslash preceding the '-' is an escape character, used to search for the literal '-'
 
 # d) Are there any believable loss-of-function variants, where a LUMPY call suggests an exon deletion
 # that is also supported by CNVNator read-depth analysis? 
 # (use the entire genome callset for this question to help chances of finding one)
-# are any of these homozygous?
+cat ../supplemental/NA12878.lumpy.bedpe \
+    | awk '{ if ($11=="DEL") print $1,$2,$6,$7 }' OFS="\t" \
+    | bedtools intersect -wa -r -f 0.5 -a stdin -b ../supplemental/NA12878.cnvnator.cnvs.bed \
+    | bedtools intersect -wo -a stdin -b ../annotations/refGene.exons.b37.bed \
+    | groupBy -g 1,2,3,4 -c 9 -o distinct
+
+# e) are any of the loss-of-function variants from (d) homozygous?
+# hint: begin by retrieving the homozygous variants from ../supplemental/NA12878.lumpy.vcf, then use
+# ~/HALL_2014/bin/zjoin to join them against the BEDPE file
+
+# retrieve the IDs of homozygous variants from the VCF
+cat ../supplemental/NA12878.lumpy.vcf | grep -v '^#' | awk '{ if ($10~"1/1") print $3 }' > homAlt.ids.txt
+
+# join the homozygous variant against the command from (d).
+cat ../supplemental/NA12878.lumpy.bedpe \
+    | awk '{ if ($11=="DEL") print $1,$2,$6,$7 }' OFS="\t" \
+    | bedtools intersect -wa -r -f 0.5 -a stdin -b ../supplemental/NA12878.cnvnator.cnvs.bed \
+    | bedtools intersect -wo -a stdin -b ../annotations/refGene.exons.b37.bed \
+    | groupBy -g 1,2,3,4 -c 9 -o distinct \
+    | zjoin -1 4 -2 1 -a stdin -b homAlt.ids.txt
